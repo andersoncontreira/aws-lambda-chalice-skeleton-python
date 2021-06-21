10b84bf2e044fd936eebee94057c375ae249949f
Anderson Contreira <anderson.contreira@gmail.com>
2021-06-21 17:08:58 -0300

    tratativa de codigo duplicado

--- chalicelib/enums/__init__.py
+++ chalicelib/enums/__init__.py
@@ -45,34 +45,8 @@ class CustomEnum(Enum):
         return None
 
 
-class CustomIntEnum(IntEnum):
-    @classmethod
-    def get_values(cls):
-        return list(map(lambda c: c.value, cls))
-
-    @classmethod
-    def get_codes(cls):
-        return list(map(lambda c: c.name, cls))
-
-    @classmethod
-    def from_value(cls, value):
-        for e in cls:
-            # print(e.value, value, e.value == value)
-            if isinstance(e.value, tuple):
-                if e.value[0] == value:
-                    return e
-                elif e.value == value:
-                    return e
-            elif e.value == value:
-                return e
-        return None
-
-    @classmethod
-    def from_code(cls, code):
-        for e in cls:
-            if e.name == code:
-                return e
-        return None
+class CustomIntEnum(CustomEnum, IntEnum):
+    """Enum where members are also (and must be) ints"""
 
 
 class CustomStringEnum(CustomEnum):
